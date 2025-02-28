e583de3946449f9f01f54efde204af1b079b31c8
MgZawHeinHtet <zawheinhtet1223@gmail.com>
2025-03-01 00:13:27 +0630

    change local env to global

--- app/_services/apiNews.ts
+++ app/_services/apiNews.ts
@@ -2,7 +2,7 @@ export async function getNews({ pageParam = 1 }) {
   console.log(process.env.API_URL);
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}?q=sport&page=${pageParam}&pageSize=6&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`
+    `https://newsapi.org/v2/everything?q=sport&page=${pageParam}&pageSize=6&apiKey=02e6d63e8fd04514882071357bb32e6b`
   );
 
   const data = await res.json();
